0438be5b9914831cae18b126eaa466ffe3852a60
JuanPZ28 <juandavidlopezyara2006@outlook.com>
2025-05-21 17:40:26 -0500

    Update Ejercicio6.hs

--- Ejercicio6.hs
+++ Ejercicio6.hs
@@ -1,14 +1,13 @@
+main :: IO ()
 main = do
     let miLista :: [Int]
-        miLista = [42, 17, 83, 9, 56, 21, 74, 3, 68, 90,
-                   12, 37, 5, 61, 28, 49, 77, 15, 36, 81,
-                   6, 24, 59, 70, 19, 95, 32, 8, 45, 63]
+        miLista = take 30 [2,4..]
 
-        pares = filter even miLista
-        suma = foldl (+) 0 miLista
+        suma = sum miLista
         promedio :: Double
-        promedio = fromIntegral suma / fromIntegral (length miLista)  -- División correcta
+        promedio = fromIntegral suma / fromIntegral (length miLista)
 
-    putStrLn $ "Números pares: " ++ show pares
+    putStrLn $ "Lista de los primeros 30 números pares: " ++ show miLista
     putStrLn $ "Suma total: " ++ show suma
     putStrLn $ "Promedio: " ++ show promedio
+
