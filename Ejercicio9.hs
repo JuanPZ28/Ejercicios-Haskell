6eb4845320858e65ef467c2efbbf6826e634546f
JuanPZ28 <juandavidlopezyara2006@outlook.com>
2025-05-21 17:53:12 -0500

    Update Ejercicio9.hs

--- Ejercicio9.hs
+++ Ejercicio9.hs
@@ -1,4 +1,4 @@
--- Función que pide 28 temperaturas y los guarda en una lista
+-- Función que pide n temperaturas y las guarda en una lista
 pedirNumeros :: Int -> IO [Int]
 pedirNumeros 0 = return []
 pedirNumeros n = do
@@ -7,22 +7,28 @@ pedirNumeros n = do
     let numero = read input :: Int
     resto <- pedirNumeros (n - 1)
     return (numero : resto)
+
 main :: IO ()
 main = do
-    putStrLn "Vas a ingresar 28 temperaturas:"
-    numeros <- pedirNumeros 28
+    putStrLn "Vas a ingresar 28 temperaturas (una por cada día):"
+    temperaturas <- pedirNumeros 28
     putStrLn "Las temperaturas ingresadas fueron:"
-    print numeros
+    print temperaturas
 
-    let suma = sum numeros
+    let suma = sum temperaturas
         promedio :: Double
-        promedio = fromIntegral suma / fromIntegral (length numeros)
+        promedio = fromIntegral suma / fromIntegral (length temperaturas)
 
-        maxima = maximum numeros
-        minima = minimum numeros
+        maxima = maximum temperaturas
+        minima = minimum temperaturas
 
-    putStrLn $ "Temperatura promedio del mes es de:" ++ show promedio
-    putStrLn $ "La temperatura maxima fue:"
-    print maxima
-    putStrLn $ "La temperatura minima fue: " 
-    print minima
+        -- Buscar la posición del primer día con temperatura máxima
+        dias = [1..28]
+        paresDiaTemp = zip dias temperaturas
+
+        diaMax = fst (head (filter (\(_, t) -> t == maxima) paresDiaTemp))
+        diaMin = fst (head (filter (\(_, t) -> t == minima) paresDiaTemp))
+
+    putStrLn $ "Temperatura promedio del mes: " ++ show promedio ++ "°C"
+    putStrLn $ "Temperatura máxima: " ++ show maxima ++ "°C (Día " ++ show diaMax ++ ")"
+    putStrLn $ "Temperatura mínima: " ++ show minima ++ "°C (Día " ++ show diaMin ++ ")"
